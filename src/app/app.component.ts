8ecfdeb4a314c0496584b569ce77dc56fe890ec0
Roberto Tienda <rohatiro@gmail.com>
2023-11-16 14:33:21 -0600

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  cards: number = 10;
+  padd: number = 10;
+
+  onScroll() {
+    this.cards += this.padd;
+    console.log(this.cards);
+  }
+
+  arraySpaces(): Array<null> {
+    return Array(this.cards);
+  }
+}
